1943cf09922cd4eff5521419bd929a122bbeac77
aliesmaelfard <aliesmaelfard@gmail.com>
2023-06-24 22:01:18 +0330

    Add Model class

---
+++ Maui.IndustriesDictionary/Model/WordsModel.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.Dictionary.Model
+{
+    [Table("words")]
+    public class WordsModel
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+
+        public string Deutsch { get; set; }
+        public string English { get; set; }
+        public string Farsi { get; set; }
+    }
+}
