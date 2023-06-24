f15164cc4aafb02de6da7ddc9777e1fe3936535b
aliesmaelfard <aliesmaelfard@gmail.com>
2023-06-24 23:00:41 +0330

    Start Desiging the UI

--- Maui.IndustriesDictionary/Repository/MyRepository.cs
+++ Maui.IndustriesDictionary/Repository/MyRepository.cs
@@ -8,7 +8,7 @@ namespace Maui.Dictionary.Repository
         private readonly SQLiteConnection _database;
 
         public static string DbPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-            "data.db");
+            "data.sqlite");
 
         public MyRepository()
         {
@@ -18,8 +18,6 @@ namespace Maui.Dictionary.Repository
 
         public  List<WordsModel> GetList(string word)
         {
-       //     int range2 = range + 20;
-        //    var x =  _database.Query<WordsModel>("Select *  FROM words Limit "+range+", "+range2+"   ;");
             var x =  _database.Query<WordsModel>($"Select *  FROM words  Where English Like '{word}%'  Limit 20");
             return x;
         }
